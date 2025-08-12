f8ccbd188214ac02df96e30b69895ea81bc9cba8
Felipe Fuentes <92124800+anfefuro@users.noreply.github.com>
2025-08-12 14:58:30 -0500

    [FIX] local address

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
+import os
 
 from ipcp import base_transformacion, actualizacion, capitalizacion, actualizacion_y_capitalizacion, input_transformacion
 
-ipcp_base = pd.read_csv('ipcp.csv')
+file_dir = f'{os.getcwd()}/'
+
+ipcp_base = pd.read_csv(f'{file_dir}ipcp.csv')
 
 ipcp_base = base_transformacion(ipcp_base)
 
