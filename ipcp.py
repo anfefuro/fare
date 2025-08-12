a4c917d9c569a3ddd4ae1025a7939e3a45876621
Felipe Fuentes <92124800+anfefuro@users.noreply.github.com>
2025-08-12 08:10:24 -0500

    [ADD] ipcp script and main base

---
+++ ipcp.py
@@ -0,0 +1,234 @@
+import pandas as pd
+import os
+import numpy as np
+
+ipcp_base = pd.read_csv('ipcp.py')
+
+def base_transformacion()
+
+ipcp_base = ipcp_base[['PERIODO\n(DD-MM-AAAA)','VALOR IPC','VALOR IPCP','FECHA INICIO','FECHA FINAL','VALOR IPCP.1','VALOR IPCP-1']]
+ipcp_base.rename(columns={
+    'PERIODO\n(DD-MM-AAAA)':'periodo',
+    'VALOR IPC':'ipc',
+    'VALOR IPCP':'ipcp',
+    'FECHA INICIO':'fecha_inicio',
+    'FECHA FINAL':'fecha_final',
+    'VALOR IPCP.1':'ipcp.1',
+    'VALOR IPCP-1':'ipcp-1'
+}, inplace=True)
+
+# ------ CAMBIOS BASE --------
+# Transformar la columna Periodo a fecha con formato date %Y-%m-%d
+ipcp_base['periodo'] = pd.to_datetime(ipcp_base['periodo'], format='%d/%m/%Y')
+ipcp_base['fecha_inicio'] = pd.to_datetime(ipcp_base['fecha_inicio'], format='%d/%m/%Y')
+ipcp_base['fecha_final'] = pd.to_datetime(ipcp_base['fecha_final'], format='%d/%m/%Y')
+# Formato de las columnas VALOR IPCP, VALOR IPCP.1, VALOR IPCP-1
+ipcp_base['ipcp'] = ipcp_base['ipcp'].str.replace(',', '').astype(float)
+ipcp_base['ipcp.1'] = ipcp_base['ipcp.1'].str.replace(',', '.').astype(float)
+ipcp_base['ipcp-1'] = ipcp_base['ipcp-1'].str.replace(',', '.').astype(float)
+
+def actualizacion(valor_actualizar, fecha_inicial, fecha_final):
+
+  # Valores usuario
+  valor_actualizar = float(valor_actualizar)
+  # Variable instanciada como fecha
+  fecha_inicial = pd.to_datetime(fecha_inicial) # '1995-04-28 00:00:00'
+  fecha_final = pd.to_datetime(fecha_final) # '2024-01-30 00:00:00'
+  # fecha_ipc_actualizar = pd.to_datetime('2023-12-31 00:00:00')
+  # Valores de fecha truncados a mes
+  fecha_inicial_mes = fecha_inicial.to_period('M').to_timestamp()
+  fecha_final_mes = fecha_final.to_period('M').to_timestamp()
+  # fecha_ipc_actualizar_mes = fecha_ipc_actualizar.to_period('M').to_timestamp()
+
+  # Definición de variables
+  # Valor a actualizar (En este ejemplo son $100.000)
+  b1 = valor_actualizar
+
+  # IPCP del mes anterior a la Fecha Final (En este ejemplo es el IPCP de diciembre de 2023)
+  fecha_final_menos_un_mes = fecha_final_mes - pd.DateOffset(months=1)
+  IPCPf = ipcp_base[ipcp_base['fecha_inicio'] == fecha_final_menos_un_mes]['ipcp'].values[0]
+
+  # IPCP del mes Fecha Final (En este caso el IPCP de enero de 2024)
+  IPCPaltf = ipcp_base[ipcp_base['fecha_inicio'] == fecha_final_mes]['ipcp'].values[0]
+
+  # Es el # de dias Calendario que tiene el mes de Fecha Final (En este caso son 31 dias (Enero 2024), en la formula se obtiene de la Hoja DiasMes)
+  inicio_mes_final = ipcp_base[ipcp_base['fecha_inicio'] == fecha_final_mes]['fecha_inicio'].values[0]
+  fin_mes_final = ipcp_base[ipcp_base['fecha_inicio'] == fecha_final_mes]['fecha_final'].values[0]
+  Dmf = int((fin_mes_final - inicio_mes_final) / np.timedelta64(1, 'D')) + 1
+
+  # Son los dias del mes final posterior al primer dia (En este caso, al ser 30 de enero; se calculan esos 30 dias adicionales de IPCP)
+  df = int((fecha_final - inicio_mes_final) / np.timedelta64(1, 'D')) + 1
+
+  # IPCP del mes anterior a la Inicial (En este ejemplo es el IPCP de marzo de 1995)
+  fecha_inicial_menos_un_mes = fecha_inicial_mes - pd.DateOffset(months=1)
+  IPCPi = ipcp_base[ipcp_base['fecha_inicio'] == fecha_inicial_menos_un_mes]['ipcp'].values[0]
+
+  # IPCP del mes Fecha Inicial (En este caso el IPCP de abril de 1995)
+  IPCPalti = ipcp_base[ipcp_base['fecha_inicio'] == fecha_inicial_mes]['ipcp'].values[0]
+
+  # Es el # de dias Calendario que tiene el mes de Fecha Inicial (En este caso son 30 dias (Abril 1995), en la formula se obtiene de la Hoja DiasMes)
+  inicio_mes_inicial = ipcp_base[ipcp_base['fecha_inicio'] == fecha_inicial_mes]['fecha_inicio'].values[0]
+  fin_mes_inicial = ipcp_base[ipcp_base['fecha_inicio'] == fecha_inicial_mes]['fecha_final'].values[0]
+  Dmi = int((fin_mes_inicial - inicio_mes_inicial) / np.timedelta64(1, 'D')) + 1
+
+  # Son los dias del mes inicial posterior al primer dia (En este caso, al ser 28 de abril; se calculan esos 28 dias adicionales de IPCP)
+  di = int((fecha_inicial - inicio_mes_inicial) / np.timedelta64(1, 'D')) + 1
+
+  # print(
+  #     f'IPCPf: {IPCPf}\n',
+  #     f'IPCPaltf: {IPCPaltf}\n',
+  #     f'Dmf: {Dmf}\n',
+  #     f'df: {df}\n',
+  #     f'IPCPi: {IPCPi}\n',
+  #     f'IPCPalti: {IPCPalti}\n',
+  #     f'Dmi: {Dmi}\n',
+  #     f'di: {di}'
+  # )
+
+  num = IPCPf + (((IPCPaltf - IPCPf) / Dmf) * df)
+  den = IPCPi + (((IPCPalti - IPCPi) / Dmi) * di)
+
+  valor_actualizado = b1 * (num / den)
+  valor_actualizado = round(valor_actualizado, 2)
+
+  return valor_actualizado
+
+def capitalizacion(valor_capitalizar, TRR, fecha_inicial, fecha_final):
+
+  # Valores Usuario
+  valor_capitalizar = float(valor_capitalizar)
+  fecha_inicial = pd.to_datetime(fecha_inicial)
+  fecha_final = pd.to_datetime(fecha_final)
+  TRR = TRR
+
+  # Variables
+  # Valor a Actualizar (En este ejemplo son $100.000)
+  B1 = valor_capitalizar
+  # Tasa de Rendimiento (Puede ser 3% o 4%) (Se expresa como entero pues en la formula se convierte en porcentaje al dividir en 100)
+  B4 = TRR
+  # Fecha Final ("En este caso excel no cuenta el primer dia, por tanto si se resta (31/12/2025-01/01/2025) es igual a 364 y no 365, y así lo calcula el MinHacienda")
+  B3 = fecha_final
+  # Fecha Inicial
+  B2 = fecha_inicial
+  # Calculo del exponente
+  exp = int((B3 - B2) / np.timedelta64(1, 'D'))
+
+  print(
+      f'B1: {B1}\n'
+      f'B2: {B2}\n'
+      f'B3: {B3}\n'
+      f'B4: {B4}\n'
+      f'exp: {exp}'
+  )
+
+  exp_cal = exp / 365.25
+  valor_capitalizado = B1 * ((1 + (B4 / 100)) ** exp_cal)
+  valor_capitalizado = round(valor_capitalizado, 2)
+
+  return valor_capitalizado
+
+def actualizacion_y_capitalizacion(valor_actualizar_capitalizar, fecha_inicial, fecha_final, TRR):
+
+  # Valores usuario
+  valor_actualizar_capitalizar = float(valor_actualizar_capitalizar)
+  fecha_inicial = pd.to_datetime(fecha_inicial)
+  fecha_final = pd.to_datetime(fecha_final)
+  TRR = float(TRR)
+  # Valores de fecha truncados a mes
+  fecha_inicial_mes = fecha_inicial.to_period('M').to_timestamp()
+  fecha_final_mes = fecha_final.to_period('M').to_timestamp()
+
+  # Valor a Actualizar (En este ejemplo son $4.000.000)
+  B1 = valor_actualizar_capitalizar
+
+  # IPCP del mes anterior a la Fecha Final (En este ejemplo es el IPCP de mayo de 2024)
+  fecha_final_menos_un_mes = fecha_final_mes - pd.DateOffset(months=1)
+  IPCP_yf_mf = ipcp_base[ipcp_base['fecha_inicio'] == fecha_final_menos_un_mes]['ipcp'].values[0]
+
+  # IPCP del mes Fecha Final (En este caso el IPCP de junio de 2024)
+  IPCPsig_yf_mf = ipcp_base[ipcp_base['fecha_inicio'] == fecha_final_mes]['ipcp'].values[0]
+
+  # Es el # de dias Calendario que tiene el mes de Fecha Final ("En este caso son 30 dias (junio 2024), en la formula se obtiene de la Hoja DiasMes")
+  inicio_mes_final = ipcp_base[ipcp_base['fecha_inicio'] == fecha_final_mes]['fecha_inicio'].values[0]
+  fin_mes_final = ipcp_base[ipcp_base['fecha_inicio'] == fecha_final_mes]['fecha_final'].values[0]
+  Dmf = int((fin_mes_final - inicio_mes_final) / np.timedelta64(1, 'D')) + 1
+
+  # Son los dias del mes Fecha Final posterior al primer dia ("En este caso, al ser 07 de junio; se calculan esos 07 dias adicionales de IPCP")
+  df = int((fecha_final - inicio_mes_final) / np.timedelta64(1, 'D')) + 1
+
+  # IPCP del mes anterior a la Inicial (En este ejemplo es el IPCP de marzo de 1995)
+  fecha_inicial_menos_un_mes = fecha_inicial_mes - pd.DateOffset(months=1)
+  IPCP_yi_mi = ipcp_base[ipcp_base['fecha_inicio'] == fecha_inicial_menos_un_mes]['ipcp'].values[0]
+
+  # IPCP del mes Fecha Inicial (En este caso el IPCP de abril de 1995)
+  IPCPsig_yi_mi = ipcp_base[ipcp_base['fecha_inicio'] == fecha_inicial_mes]['ipcp'].values[0]
+
+  # Es el # de dias Calendario que tiene el mes de Fecha Inicial ("En este caso son 30 dias (Abril 1995), en la formula se obtiene de la Hoja DiasMes")
+  inicio_mes_inicial = ipcp_base[ipcp_base['fecha_inicio'] == fecha_inicial_mes]['fecha_inicio'].values[0]
+  fin_mes_inicial = ipcp_base[ipcp_base['fecha_inicio'] == fecha_inicial_mes]['fecha_final'].values[0]
+  Dmi = int((fin_mes_inicial - inicio_mes_inicial) / np.timedelta64(1, 'D')) + 1
+
+  # Son los dias del mes Fecha Inicial posterior al primer dia ("En este caso, al ser 28 de abril; se calculan esos 28 dias adicionales de IPCP")
+  di = int((fecha_inicial - inicio_mes_inicial) / np.timedelta64(1, 'D')) + 1
+
+  # Tasa de Rendimiento (Puede ser 3% o 4%) (Se expresa como entero pues en la formula se convierte en porcentaje al dividir en 100)
+  B4 = TRR
+  # Fecha Final ("En este caso excel no cuenta el primer dia, por tanto si se resta (31/12/2025-01/01/2025) es igual a 364 y no 365, y así lo calcula el MinHacienda")
+  B3 = fecha_final
+  # Fecha Inicial
+  B2 = fecha_inicial
+  # Calculo del exponente
+  exp = float((B3 - B2) / np.timedelta64(1, 'D'))
+
+  # print(
+  #     f'B1: {B1}\n',
+  #     f'IPCP_yf_mf: {IPCP_yf_mf}\n',
+  #     f'IPCPsig_yf_mf: {IPCPsig_yf_mf}\n',
+  #     f'Dmf: {Dmf}\n',
+  #     f'df: {df}\n',
+  #     f'IPCP_yi_mi: {IPCP_yi_mi}\n',
+  #     f'IPCPsig_yi_mi: {IPCPsig_yi_mi}\n',
+  #     f'Dmi: {Dmi}\n',
+  #     f'di: {di}\n',
+  #     f'B4: {B4}\n',
+  #     f'B3: {B3}\n',
+  #     f'B2: {B2}\n',
+  #     f'exp: {exp}'
+  # )
+
+  num = IPCP_yf_mf + (((IPCPsig_yf_mf - IPCP_yf_mf) / Dmf) * df)
+  dem = IPCP_yi_mi + (((IPCPsig_yi_mi - IPCP_yi_mi) / Dmi) * di)
+  mulp = (1 + (B4 / 100)) ** (exp / 365.25)
+
+  valor_actualizado_capitalizado = (B1 * (num / dem)) * mulp
+  valor_actualizado_capitalizado = round(valor_actualizado_capitalizado, 2)
+
+  return valor_actualizado_capitalizado
+
+json_data = """
+[
+  {"valor": 7300082, "fecha_final": "1995-01-01", "tipo": "Inicial", "trr": 4},
+  {"valor": null, "fecha_final": "2005-12-09", "tipo": "Pension", "trr": 4},
+  {"valor": 9068000, "fecha_final": "2005-12-14", "tipo": "Abono", "trr": 4},
+  {"valor": 19352000, "fecha_final": "2006-01-26", "tipo": "Abono", "trr": 4},
+  {"valor": 2500000, "fecha_final": "2008-09-01", "tipo": "Reintegro", "trr": 4},
+  {"valor": null, "fecha_final": "2025-06-30", "tipo": "Pago", "trr": 4}
+]
+"""
+
+ejemplo = pd.read_json(json_data)
+
+# Se genera la columna fecha final y valor anterior
+ejemplo = ejemplo.sort_values('fecha_final').reset_index(drop=True)
+ejemplo['fecha_inicial'] = ejemplo['fecha_final'].shift(1)
+ejemplo['valor'] = ejemplo['valor'].fillna(0)
+ejemplo['valor_anterior'] = ejemplo['valor'].shift(1)
+ejemplo['valor_anterior'] = ejemplo['valor_anterior'].fillna(0)
+ejemplo['tipo_anterior'] = ejemplo['tipo'].shift(1)
+
+try:
+  ejemplo['fecha_pension'] = ejemplo[ejemplo['tipo'] == 'Pension']['fecha_final'].values[0]
+except:
+  ejemplo['fecha_pension'] = ejemplo[ejemplo['tipo'] == 'Pago']['fecha_final'].values[0]
+
+ejemplo['valor_inicial'] = ejemplo[ejemplo['tipo'] == 'Inicial']['valor'].values[0]
