257ef5acb2ece38f1b187d95d4ba44c5fd95a64b
Felipe Fuentes <92124800+anfefuro@users.noreply.github.com>
2025-08-12 14:56:11 -0500

    [FIX] csv reference

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from ipcp import base_transformacion, actualizacion, capitalizacion, actualizacion_y_capitalizacion, input_transformacion
 
-ipcp_base = pd.read_csv('ipcp.py')
+ipcp_base = pd.read_csv('ipcp.csv')
 
 ipcp_base = base_transformacion(ipcp_base)
 
